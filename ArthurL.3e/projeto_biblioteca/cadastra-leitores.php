ad14204944b4e0a1b115eed6f9fb0d9d7f4bc806
IAL3MI <165719277+IAL3MI@users.noreply.github.com>
2024-10-21 15:29:27 -0300

    Add files via upload

--- ArthurL.3e/projeto_biblioteca/cadastra-leitores.php
+++ ArthurL.3e/projeto_biblioteca/cadastra-leitores.php
@@ -6,19 +6,19 @@ if (!$conn){
     die ("Falha na conexão: " . mysqli_connect_error());
 }
 
-$nome = '';
-$dtnac = '';
-$celular = '';
-$email = '';
-$ra = '';
-$endereco = '';
-$num_end = '';
-$bairro = '';
-$cidade = '';
+$nome = "$_POST[name]";
+$dtnasc = "$_POST[dtnasc]";
+$celular = "$_POST[celular]";
+$email = "$_POST[email]";
+$ra = "$_POST[ra]";
+$endereco = "$_POST[endereco]";
+$num_end = "$_POST[num_end]";
+$bairro = "$_POST[bairro]";
+$cidade = "$_POST[cidade]";
 
 $sql = "INSERT INTO `leitores`
 (Nome, Dtnasc, Celular, Email, RA, Endereco, NumEnd, Bairro, CidadeUF) 
-VALUES('$nome','$dtnac','$celular', '$email', '$ra','$endereco','$num_end','$bairro','$cidade')";
+VALUES('$nome','$dtnasc','$celular', '$email', '$ra','$endereco','$num_end','$bairro','$cidade')";
 
 $query = mysqli_query(mysql: $conn,query: $sql) or
 die (mysqli_error(mysql:$conn));
@@ -26,7 +26,7 @@ die (mysqli_error(mysql:$conn));
 if($query){
     echo "<center>";
     echo "Cadastro feito com sucesso! <br>";
-    echo "<a href='index.php'><button title ='Home Page'>Voltar</button></a>";
+    echo "<a href='cadastrar.html'><button title ='Home Page'>Voltar</button></a>";
     echo "</center>";
 } else {
     echo "<center>";
